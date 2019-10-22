5a80a1258f1ac017ae0853eec3e2fcd89f3914dc
happywhy0928 <56148369+happywhy0928@users.noreply.github.com>
2019-10-22 01:30:36 -0700

    Add files via upload

---
+++ src/src/DictionaryTrie/DictionaryTrie.hpp
@@ -0,0 +1,61 @@
+/**
+ * TODO: File Header
+ *
+ * Author:
+ */
+#ifndef DICTIONARY_TRIE_HPP
+#define DICTIONARY_TRIE_HPP
+
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+/**
+ * The class for a dictionary ADT, implemented as either
+ * a mulit-way trie or a ternary search tree.
+ */
+class DictionaryTrie {
+  private:
+    // TODO: add private members and helper methods here
+    class TierNode {
+      public:
+        char singleChar;
+        int frequency;
+        TierNode* left;
+        TierNode* median;
+        TierNode* right;
+
+        TierNode(int frequency, char singleChar)
+            : frequency(frequency), singleChar(singleChar) {}
+        //  left(nullptr),
+        // median(nullptr),
+        // right(nullptr)
+    };
+    TierNode* root;
+    void deleteAll(TierNode* node);
+
+  public:
+    /* TODO: add function header */
+    DictionaryTrie();
+
+    /* TODO: add function header */
+    bool insert(string word, unsigned int freq);
+
+    /* TODO: add function header */
+    bool find(string word) const;
+
+    /* TODO: add function header */
+    vector<string> predictCompletions(string prefix,
+                                      unsigned int numCompletions);
+
+    /* TODO: add function header */
+    vector<string> predictUnderscores(string pattern,
+                                      unsigned int numCompletions);
+
+    /* TODO: add function header */
+    ~DictionaryTrie();
+};
+
+#endif  // DICTIONARY_TRIE_HPP
